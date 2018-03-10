9a847d9112bc481ad822cb8cdec19988e05975dd
F-loat <945852046@qq.com>
2018-03-11 01:01:44 +0800

    圈子列表页基本完成

--- src/pages/quanzi/index.vue
+++ src/pages/quanzi/index.vue
@@ -1,22 +1,85 @@
 <template lang="pug">
-.container 开发中...
+.container
+  .topic-wrap
+    a.topic-item(
+      :href="topic.link",
+      v-for="topic of topics",
+      :key="topic.id")
+      .topic-title {{topic.c}} {{topic.t}}
+      .topic-info
+        .topic-info-item {{topic.un}}
+        .topic-info-item {{topic.cn}}
+        .topic-info-item
+          img.topic-info-icon(src="/static/assets/quan_hit.png")
+          span.topic-info-text {{topic.vc}}
+        .topic-info-item
+          img.topic-info-icon(src="/static/assets/quan_comment.png")
+          span.topic-info-text {{topic.rc}}
 </template>
 
 <script>
+import api from '@/utils/api'
+
 export default {
   data () {
     return {
+      loading: false,
+      topics: []
     }
   },
-  methods: {
+  mounted () {
+    this.getTopics()
   },
-  created () {
+  onReachBottom () {
+    this.getTopics()
+  },
+  methods: {
+    async getTopics () {
+      if (this.loading) return
+      this.loading = true
+      wx.showNavigationBarLoading()
+      const { topics } = this
+      let rt = Date.now()
+      if (topics.length) {
+        const lastTopic = topics[topics.length - 1]
+        rt = lastTopic.rt.replace(/[^0-9]/ig, '')
+      }
+      const newTopics = await api.getTopics(rt)
+      newTopics.forEach(topic => {
+        const { id, c, t, un, vc } = topic
+        topic.link = `/pages/qdetail/qdetail?id=${id}&title=${c} ${t}&author=${un}&vc=${vc}`
+      })
+      this.topics = this.topics.concat(newTopics)
+      wx.hideNavigationBarLoading()
+      this.loading = false
+    }
   }
 }
 </script>
 
 <style scoped>
-.container {
-  justify-content: center;
+.topic-item {
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+}
+.topic-title {
+  font-size: 18px;
+}
+.topic-info {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 5px;
+}
+.topic-info-item {
+  margin-left: 10px;
+  font-size: 12px;
+  color: #aaa;
+  display: flex;
+  align-items: center;
+}
+.topic-info-icon {
+  width: 15px;
+  height: 15px;
 }
 </style>
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,10 @@ const api = {
   getNewslist: (r) => request(`https://api.ithome.com/json/newslist/news?r=${r}`),
   getNews: (id) => request(`https://api.ithome.com/xml/newscontent/${id}.xml`),
   getRelatedNews: (id) => request(`https://api.ithome.com/json/tags/0350/${id}.json`),
-  getSlides: () => request('https://api.ithome.com/xml/slide/slide.xml')
+  getSlides: () => request('https://api.ithome.com/xml/slide/slide.xml'),
+  getTopics: (r) => request(`https://apiquan.ithome.com/api/post?categoryid=0&type=0&orderTime=${r}&visistCount&pageLength`),
+  getTopic: (id) => request(`https://apiquan.ithome.com/api/post/${id}`),
+  getTopicComments: (id, last) => request(`https://apiquan.ithome.com/api/reply?postid=${id}&replyidlessthan=${last}`)
 }
 
 export default api
