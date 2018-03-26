ef86d475055506f4c86a22914e1cd29f0e1acc5a
F-loat <945852046@qq.com>
2018-03-26 23:36:33 +0800

    提取 topic-item 组件

---
+++ src/components/topic-item.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+a.topic-item(:href="topic.link")
+  img.topic-headimg(src="/static/assets/avatar_default.png")
+  img.topic-headimg(:src="topic.author.headimg")
+  .topic-title {{topic.tag}} {{topic.title}}
+  .topic-info
+    .topic-info-item {{topic.author.nickname}}
+    .topic-info-item {{topic.type}}
+    .topic-info-item
+      img.topic-info-icon(src="/static/assets/quan_hit.png")
+      span.topic-info-text {{topic.viewcount}}
+    .topic-info-item
+      img.topic-info-icon(src="/static/assets/quan_comment.png")
+      span.topic-info-text {{topic.replycount}}
+</template>
+
+<script>
+export default {
+  props: {
+    topic: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.topic-item {
+  padding: 10px 10px 10px 50px;
+  border-bottom: 1px solid #eee;
+  position: relative;
+}
+.topic-headimg {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  position: absolute;
+  left: 10px;
+  top: 10px;
+}
+.topic-title {
+  font-size: 16px;
+}
+.topic-info {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 10px;
+}
+.topic-info-item {
+  margin-left: 10px;
+  font-size: 12px;
+  color: #aaa;
+  display: flex;
+  align-items: center;
+}
+.topic-info-icon {
+  width: 15px;
+  height: 15px;
+  margin-right: 4px;
+}
+</style>
--- src/pages/quanzi/list/index.vue
+++ src/pages/quanzi/list/index.vue
@@ -1,29 +1,21 @@
 <template lang="pug">
 .container
   .topic-wrap
-    a.topic-item(
-      :href="topic.link",
+    topicItem(
       v-for="topic of topics",
+      :topic="topic"
       :key="topic.id")
-      img.topic-headimg(src="/static/assets/avatar_default.png")
-      img.topic-headimg(:src="topic.headimg")
-      .topic-title {{topic.c}} {{topic.t}}
-      .topic-info
-        .topic-info-item {{topic.un}}
-        .topic-info-item {{topic.cn}}
-        .topic-info-item
-          img.topic-info-icon(src="/static/assets/quan_hit.png")
-          span.topic-info-text {{topic.vc}}
-        .topic-info-item
-          img.topic-info-icon(src="/static/assets/quan_comment.png")
-          span.topic-info-text {{topic.rc}}
 </template>
 
 <script>
 import wx from 'wx'
 import { mapState, mapActions } from 'vuex'
+import topicItem from '@/components/topic-item'
 
 export default {
+  components: {
+    topicItem
+  },
   computed: {
     ...mapState([
       'topics'
@@ -54,38 +46,4 @@ export default {
 </script>
 
 <style lang="less">
-.topic-item {
-  padding: 10px 10px 10px 50px;
-  border-bottom: 1px solid #eee;
-  position: relative;
-}
-.topic-headimg {
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  position: absolute;
-  left: 10px;
-  top: 10px;
-}
-.topic-title {
-  font-size: 16px;
-}
-.topic-info {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  margin-top: 10px;
-}
-.topic-info-item {
-  margin-left: 10px;
-  font-size: 12px;
-  color: #aaa;
-  display: flex;
-  align-items: center;
-}
-.topic-info-icon {
-  width: 15px;
-  height: 15px;
-  margin-right: 4px;
-}
 </style>
