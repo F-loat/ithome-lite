9e517e367c7aeb4e65158ada897695e7c7349eda
F-loat <chaimaoyuan@foxmail.com>
2018-10-18 14:36:29 +0800

    chore: 优化打包配置

---
+++ webpack.conf.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  resolve: {
+    alias: {
+      flyio: 'flyio/dist/npm/fly',
+      wx: path.join(__dirname, 'src/utils/wx')
+    }
+  },
+  devServer: {
+    proxy: {
+      '/apiquan/api': {
+        target: 'https://apiquan.ithome.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/apiquan': ''
+        }
+      },
+      '/api': {
+        target: 'https://api.ithome.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+      '/dyn': {
+        target: 'https://dyn.ithome.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/dyn': ''
+        }
+      }
+    }
+  }
+}
