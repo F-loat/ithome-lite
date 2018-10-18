94dce24cd0b193c73e9a62a7cf525b732b8b1cf5
F-loat <chaimaoyuan@foxmail.com>
2018-10-18 10:14:47 +0800

    chore: 更新 eslint 配置

--- webpack.conf.js
+++ webpack.conf.js
@@ -7,5 +7,18 @@ module.exports = {
       flyio: 'flyio/dist/npm/wx',
       wx: path.join(__dirname, 'src/utils/wx')
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: path.resolve('src'),
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      }
+    ]
   }
 }
