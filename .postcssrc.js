9dc64e748dbc4d119ae45d2562532c023ba55f43
F-loat <chaimaoyuan@foxmail.com>
2018-10-20 00:03:27 +0800

    fix: 样式转换配置

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,11 +1,14 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
-module.exports = {
+module.exports = process.env.MODE === 'h5' ? {
   "plugins": {
-    "postcss-mpvue-wxss": {},
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
     "autoprefixer": {}
+  }
+} : {
+  "plugins": {
+    "postcss-mpvue-wxss": {}
   }
 }
