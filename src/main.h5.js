f5eb561c3021fe28dec71b7adefd18927a34731d
F-loat <chaimaoyuan@foxmail.com>
2018-10-21 22:08:44 +0800

    feat: 整合 App.vue 文件

--- src/main.h5.js
+++ src/main.h5.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Toast from 'vue2-toast'
 import { Swiper, Slide } from 'vue-swiper-component'
+import PullTo from 'vue-pull-to'
 import router from '@/router'
 import store from '@/store'
-import App from '@/App.h5'
+import App from '@/App'
 import 'minireset.css'
 import 'vue2-toast/lib/toast.css'
 
 Vue.use(Toast)
+Vue.component('PullTo', PullTo)
 Vue.component('Swiper', Swiper)
 Vue.component('SwiperItem', Slide)
 Vue.config.productionTip = false
