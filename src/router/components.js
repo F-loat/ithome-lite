d46642b1cecf6a655c25e314f53d43f903b6077c
F-loat <chaimaoyuan@foxmail.com>
2018-10-21 21:34:44 +0800

    feat: 优化路由配置

---
+++ src/router/components.js
@@ -0,0 +1,13 @@
+const pagesNewsList = () => import('@/pages/news/list')
+const pagesNewsDetail = () => import('@/pages/news/detail')
+const pagesNewsComment = () => import('@/pages/news/comment')
+const pagesQuanziList = () => import('@/pages/quanzi/list')
+const pagesQuanziDetail = () => import('@/pages/quanzi/detail')
+
+export {
+  pagesNewsList,
+  pagesNewsDetail,
+  pagesNewsComment,
+  pagesQuanziList,
+  pagesQuanziDetail
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,71 +1,19 @@
-/**
- * 有 bug，生产环境勿用
- */
-
 import Vue from 'vue'
 import Router from 'vue-router'
+import routes from './routes'
+import * as components from './components'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      alias: '/',
-      path: '/pages/news/list',
-      name: 'NewsList',
-      component: () => import('@/pages/news/list'),
-      meta: {
-        nav: true
-      },
-      config: {
-        enablePullDownRefresh: true
-      }
-    },
-    {
-      path: '/pages/news/detail',
-      name: 'NewsDetail',
-      component: () => import('@/pages/news/detail'),
-      meta: {
-        nav: false
-      },
-      config: {
-        navigationBarTitleText: '文章详情'
-      }
-    },
-    {
-      path: '/pages/news/comment',
-      name: 'NewsComment',
-      component: () => import('@/pages/news/comment'),
-      meta: {
-        nav: false
-      },
-      config: {
-        navigationBarTitleText: '评论列表'
-      }
-    },
-    {
-      path: '/pages/quanzi/list',
-      name: 'QuanziList',
-      component: () => import('@/pages/quanzi/list'),
-      meta: {
-        nav: true
-      },
-      config: {
-        navigationBarTitleText: '圈子',
-        enablePullDownRefresh: true
-      }
-    },
-    {
-      path: '/pages/quanzi/detail',
-      name: 'QuanziDetail',
-      component: () => import('@/pages/quanzi/detail'),
-      meta: {
-        nav: false
-      },
-      config: {
-        navigationBarTitleText: '圈子详情'
-      }
+  routes: routes.map(route => {
+    const name = route.path
+      .replace(/^\//, '')
+      .replace(/\/(\w)/g, (match, $1) => $1.toUpperCase())
+    return {
+      ...route,
+      component: components[name]
     }
-  ],
+  }),
   mode: 'history'
 })
