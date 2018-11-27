546b3fb4d1cc3c3f77dbccee7e4fe9fa208b6a53
F-loat <chaimaoyuan@foxmail.com>
2018-11-27 17:20:22 +0800

    chore: 更新打包配置

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,24 +1,46 @@
 const path = require('path')
 
-module.exports = {
+const config = process.env.MODE === 'mp' ? {
   resolve: {
     alias: {
       vue: 'mpvue',
       flyio: 'flyio/dist/npm/wx',
       wx: path.join(__dirname, 'src/utils/wx')
     }
+  }
+} : {
+  entry: './src/main.h5.js',
+  resolve: {
+    alias: {
+      flyio: 'flyio/dist/npm/fly',
+      wx: path.join(__dirname, 'src/utils/wx')
+    }
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        include: path.resolve('src'),
-        options: {
-          formatter: require('eslint-friendly-formatter')
+  devServer: {
+    proxy: {
+      '/apiquan/api': {
+        target: 'https://apiquan.ithome.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/apiquan': ''
+        }
+      },
+      '/api': {
+        target: 'https://api.ithome.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+      '/dyn': {
+        target: 'https://dyn.ithome.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/dyn': ''
         }
       }
-    ]
+    }
   }
 }
+
+module.exports = config
