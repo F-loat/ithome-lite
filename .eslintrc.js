c05590c6339349fefa05ffa271442351c09c96c3
F-loat <chaimaoyuan@foxmail.com>
2018-12-17 17:50:49 +0800

    chore: 调整 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,37 @@
-// https://eslint.org/docs/user-guide/configuring
+// http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint'
+    sourceType: 'module'
   },
   env: {
-    browser: true,
+    browser: false,
+    node: true,
+    es6: true
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'html'
   ],
   // add your custom rules here
-  rules: {
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
     // allow async-await
-    'generator-star-spacing': 'off',
+    'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  },
+  globals: {
+    App: true,
+    Page: true,
+    wx: true,
+    getApp: true,
+    getPage: true,
+    requirePlugin: true
   }
 }
