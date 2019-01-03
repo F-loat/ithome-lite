752eb5f5de2b82a7817a298eccb0649910515917
F-loat <chaimaoyuan@foxmail.com>
2019-01-04 00:02:11 +0800

    chore: 统一缩进格式

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,26 @@
 const plugins = []
+
 process.UNI_LIBRARIES = process.UNI_LIBRARIES || ['@dcloudio/uni-ui']
+
 process.UNI_LIBRARIES.forEach(libraryName => {
-    plugins.push([
-        'import',
-        {
-            'libraryName': libraryName,
-            'customName': (name) => {
-                return `${libraryName}/lib/${name}/${name}`
-            }
-        }
-    ])
+  plugins.push([
+    'import',
+    {
+      'libraryName': libraryName,
+      'customName': (name) => {
+        return `${libraryName}/lib/${name}/${name}`
+      }
+    }
+  ])
 })
 module.exports = {
-    presets: [
-        [
-            '@vue/app',
-            {
-                useBuiltIns: 'entry'
-            }
-        ]
-    ],
-    plugins
+  presets: [
+    [
+      '@vue/app',
+      {
+        useBuiltIns: 'entry'
+      }
+    ]
+  ],
+  plugins
 }
--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,5 @@
 const pkg = require('./package.json')
+
 module.exports = {
 	plugins: [
 		require('autoprefixer')({
